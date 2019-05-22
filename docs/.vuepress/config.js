e169536806f663b1e44868d1a9e8b1e46b1b2d04
liugq <liugq5713@gmail.com>
2019-05-22 11:27:36 +0800

    docs[article]

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,16 @@ module.exports = {
   title: 'drawer',
   description: '如何写好一个组件实战',
   base: '/drawer/',
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   themeConfig: {
+    repo: 'https://github.com/Liugq5713/drawer',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: 'GitHub',
+    docsDir: 'docs',
+    docsBranch: 'master',
+    editLinks: true,
+    editLinkText: '帮助我修改页面',
     sidebar: ['doc', '/article'],
     sidebarDepth: 2
   }
